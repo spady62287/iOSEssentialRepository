64ab7c3d54747f1e9b9b7f88ccfa505c29251809
Daniel Spady <danielspady@gmail.com>
2024-07-30 10:53:46 -0400

    Create init Methods for each type
    - create init for Friend
    - create init for Card
    - create init for Transfer

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -200,6 +200,48 @@ class ListViewController: UITableViewController {
 struct ItemViewModel {
     let title: String
     let subtitle: String
+    
+    init(_ item: Any, longDateStyle: Bool) {
+        if let friend = item as? Friend {
+            self.init(friend: friend)
+        } else if let card = item as? Card {
+            self.init(card: card)
+        } else if let transfer = item as? Transfer {
+            self.init(transfer: transfer, longDateStyle: longDateStyle)
+        } else {
+            fatalError("unknown item: \(item)")
+        }
+    }
+    
+    init(friend: Friend) {
+        title = friend.name
+        subtitle = friend.phone
+    }
+    
+    init(card: Card) {
+        title = card.number
+        subtitle = card.holder
+    }
+    
+    init(transfer: Transfer, longDateStyle: Bool) {
+        let numberFormatter = Formatters.number
+        numberFormatter.numberStyle = .currency
+        numberFormatter.currencyCode = transfer.currencyCode
+        
+        let amount = numberFormatter.string(from: transfer.amount as NSNumber)!
+        title = "\(amount) • \(transfer.description)"
+        
+        let dateFormatter = Formatters.date
+        if longDateStyle {
+            dateFormatter.dateStyle = .long
+            dateFormatter.timeStyle = .short
+            subtitle = "Sent to: \(transfer.recipient) on \(dateFormatter.string(from: transfer.date))"
+        } else {
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            subtitle = "Received from: \(transfer.sender) on \(dateFormatter.string(from: transfer.date))"
+        }
+    }
 }
 
 extension UITableViewCell {
